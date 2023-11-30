65124363dcc44f9668a813beddb459c4c1e87962
Dome1992 <forlanodomenico92@mail.com>
2023-11-30 12:56:35 +0100

    esercizio svolto, ma aperto ciclo infinito

---
+++ script.js
@@ -0,0 +1,17 @@
+//Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+
+
+
+
+
+// creare array vuoto per la spesa
+let arrSpesa = ["peperoni", "uova", "zucchine", "formaggio"];
+console.log(arrSpesa);
+
+// ciclo while
+let i = 0;
+
+while (i < arrSpesa.length) {
+    console.log(arrSpesa[i]);
+    i++;
+}
